b2b197844b34c3973cd15ad8f93d7ee33155615a
OBEXXX <49529501+OBEXXX@users.noreply.github.com>
2019-04-11 18:17:34 +0100

    5 quase feito, 6 feito, 7 quase feito, 8 feito

--- Exercícios/Exercicios_Assembly.asm
+++ Exercícios/Exercicios_Assembly.asm
@@ -2,14 +2,43 @@
 a) muda o valor de ax para bx (todos os 16 bits)
  ax=FF05H
  
-b) muda o valor de cx para 37H
+b) muda o valor de cl(8 menos significativos) para 37H
 
 c) muda o conteudo de 246BH para cx
   cx= FFH
   
-d)muda o conteudo de cx para 246BH
+d)muda o cx para 246BH
 
-e)
+e)move conteudo de bx para ax
+ax=0DH
+f)
+ cx=1FF5H
+g) multiplica BX
+ 
+ 6.
+ a) adiciona al a bl
+    move bl para o registo de memoria de 0004H
+	[0004]=FAH
+ b)
+ muda valor de bx para 0004H
+ al=FFH
+ subtrai a al, cl
+ al= 1AH
+ incrementa bx
+ bx=0005H
+ [bx]=1AH
+
+
+7.
+
+a)
+ first DB 25H
+second DB 31H
+greater DB ?
+b)
+1Byte
+c)
+first=
  
 8.
 
